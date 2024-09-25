5f798ccd7c7437ee897565316b2b24364a9c3332
ankit2006bhatia <priyanshi300508@gmail.com>
2024-09-25 14:16:36 +0530

    hi

---
+++ javaankit.js
@@ -0,0 +1,23 @@
+function vote (){
+    var name = document.getelementbyid("name").value:
+    var pass = 100* math.random();
+    var result = document.getelementbyid("result");
+    result.innerHTML= name +  "you have scored :  "
+    
+    if( marks >=0 && <=35)
+    {
+        resultmessage= name + "failed"+ marks +("marks");
+
+    }
+    if( marks >=60 && <=80)
+        {
+            resultmessage= name + "he has passed"+ marks +("marks");
+    
+        }
+        if( marks >=0 && <=35)
+            {
+                resultmessage= name + ""+ marks +("marks");
+        
+            }
+    }
+    
